2ba7df242dc3acddec32493a9ccdd594c26dd700
Vojtech Kaiser <vksmail95@gmail.com>
2023-06-25 19:33:40 +0200

    FE utils to interact with SC

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,1 +1,4 @@
-export const contractAddress = "0xF634D3896d95Ae28CE611baE7EB0e8297c62b8F1"
+import abi from './contractAbi.json'
+
+export const contractAddress = "0xAC03814d16c6ACeB2c469Dab1a68d67884824F3F" // "0xF634D3896d95Ae28CE611baE7EB0e8297c62b8F1"
+export const contractAbi = abi.result
