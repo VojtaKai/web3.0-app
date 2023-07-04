571b85b726d301091dcea501bd09cf2a13348d70
Vojtech Kaiser <vksmail95@gmail.com>
2023-07-04 23:54:30 +0200

    shortened address to ethereum card

--- client/src/components/EthereumCard.tsx
+++ client/src/components/EthereumCard.tsx
@@ -2,6 +2,8 @@ import {LiaEthereum} from 'react-icons/lia'
 import {IoMdInformationCircle} from 'react-icons/io'
 import {FaMoneyCheckDollar} from 'react-icons/fa6'
 import { IconContext } from 'react-icons';
+import { useTransactionContext } from '../context/TransactionContext';
+import { shortenAddress } from '../utils/shortenAddress';
 
 interface WhitenedElementProps {
     El: string
@@ -19,6 +21,13 @@ function WhitenedElement({El, stylingProps}: WhitenedElementProps) {
   }
 
 export const EthereumCard = () => {
+
+    const context = useTransactionContext()
+
+    if (!context) {
+        return
+    }
+    const {currentAccount} = context
     return (
         <div className='bg-red border-2 border-black w-72 rounded-xl pl-2 blue-ethereum-card p-2'>
             <ul className='flex pb-10 justify-between'>
@@ -30,7 +39,12 @@ export const EthereumCard = () => {
                 </li>
             </ul>
 
-            <h3 className='text-black text-lg'>...</h3>
+            {
+                currentAccount ? 
+                <h3 className='text-black text-md'>{shortenAddress(currentAccount)}</h3>
+                :
+                <h3 className='text-black text-lg'>...</h3>
+            }
             <ul className='flex justify-between items-center'>
                 <li className='text-black font-bold text-lg'>Ethereum</li>
                 <li>
