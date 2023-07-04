7e1e011dfb835da153c4b975fb77f09f5ed0c3f4
Vojtech Kaiser <vksmail95@gmail.com>
2023-07-04 19:54:53 +0200

    redeploy of the smart contract

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,4 +1,4 @@
 import abi from './contractAbi.json'
 
-export const contractAddress = "0xAC03814d16c6ACeB2c469Dab1a68d67884824F3F" // "0xF634D3896d95Ae28CE611baE7EB0e8297c62b8F1"
+export const contractAddress = "0x0156e7163931C144DF7300a84EA174D175545B16" // "0xAC03814d16c6ACeB2c469Dab1a68d67884824F3F" // "0xF634D3896d95Ae28CE611baE7EB0e8297c62b8F1"
 export const contractAbi = abi.result
--- smart_contract/src/Transactions.sol
+++ smart_contract/src/Transactions.sol
@@ -23,4 +23,8 @@ contract Transactions {
 
         emit Transfer(msg.sender, receiver, amount, message, block.timestamp, keyword);
     }
+
+    function getAllTransactions() public view returns(Transaction[] memory) {
+        return transactions;
+    }
 }
